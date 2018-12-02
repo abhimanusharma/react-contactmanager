322d91d6fc9a7c17a2b0ea7786311c0a476746ec
Abhimanu Sharma <dragonabv1@gmail.com>
2018-12-02 22:44:37 +0530

    React component lift cycle study test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/layout/Header";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 import { Provider } from "./context";
+import Test from "./components/Test/Test";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
               </Switch>
             </div>
