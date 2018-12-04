151d5e4a26037258f7b29b38d8c65f9637ac1253
Abhimanu Sharma <dragonabv1@gmail.com>
2018-12-04 18:49:38 +0530

    DELETE request from api using axios

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Consumer } from "../../context";
+import axios from "axios";
 
 class Contact extends Component {
   state = {
@@ -8,7 +9,9 @@ class Contact extends Component {
   };
 
   onClickDelete = (id, dispatch) => {
-    dispatch({ type: "DELETE_CONTACT", payload: id });
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res => dispatch({ type: "DELETE_CONTACT", payload: id }));
   };
 
   render() {
