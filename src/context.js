dc2fab9cd1d0860e1dce86ffe83bc5bb347e3815
Abhimanu Sharma <dragonabv1@gmail.com>
2018-12-04 15:41:06 +0530

    HTTP GET Request from api

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -23,30 +24,17 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: "Jon Doe",
-        email: "jdoe@gmail.com",
-        phone: "999999999"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "kwilliams@gmail.com",
-        phone: "123231231"
-      },
-      {
-        id: 3,
-        name: "Henary Johnson",
-        email: "hjonson@gmail.com",
-        phone: "2345599900"
-      }
-    ],
+    contacts: [],
     dispatch: action => {
       this.setState(state => reducer(state, action));
     }
   };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(res => this.setState({ contacts: res.data }));
+  }
 
   render() {
     return (
