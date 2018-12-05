b81fd7a518b35c84045ba06ed329e348763119f8
Abhimanu Sharma <dragonabv1@gmail.com>
2018-12-06 00:32:08 +0530

    Updates

---
+++ precache-manifest.14ca6f120fd33c58fee8b97343736ba9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "82ec5eed5e56fc4b2339",
+    "url": "/contactmanager/static/js/main.82ec5eed.chunk.js"
+  },
+  {
+    "revision": "bd6ffd1d1ba572011f5b",
+    "url": "/contactmanager/static/js/1.bd6ffd1d.chunk.js"
+  },
+  {
+    "revision": "82ec5eed5e56fc4b2339",
+    "url": "/contactmanager/static/css/main.fdba9fc5.chunk.css"
+  },
+  {
+    "revision": "bd6ffd1d1ba572011f5b",
+    "url": "/contactmanager/static/css/1.b91d3682.chunk.css"
+  },
+  {
+    "revision": "6e3cfbe4e25a5346d39f75a7948b0e98",
+    "url": "/contactmanager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-contactmanager/precache-manifest.768fe8f0a43a1d61d5d39cfd1b1b77c7.js"
+  "/contactmanager/precache-manifest.14ca6f120fd33c58fee8b97343736ba9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-contactmanager/index.html", {
+workbox.routing.registerNavigationRoute("/contactmanager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
