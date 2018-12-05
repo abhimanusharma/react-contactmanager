51bb8216c14766bb5c428a9913eec93d8d97e1fa
Abhimanu Sharma <dragonabv1@gmail.com>
2018-12-05 23:53:17 +0530

    Deployment changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className="App">
             <Header branding="Contact Manager" />
             <div className="container">
